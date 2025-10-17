a3d65e8595f9bdf5ae4f3db5c1939a07a29384a9
B-priyam <priyam3801h@gmail.com>
2025-10-17 12:25:22 +0530

    theme and styling

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,12 @@
 import { LoginForm } from "@/features/auth/components/login-form";
 import { requireUnAuth } from "@/lib/auth-utils";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const page = async () => {
   await requireUnAuth();
-  return (
-    <div className="">
-      <LoginForm />
-    </div>
-  );
+  return <LoginForm />;
 };
 
 export default page;
