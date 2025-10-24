71fc8c4586331bd055af244d62e6de0a9c39db66
B-priyam <priyam3801h@gmail.com>
2025-10-24 13:10:59 +0530

    background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import { requireAuth } from "@/lib/auth-utils";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 
-const page = async () => {
-  await requireAuth();
+const page = () => {
+  // await requireAuth();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
   // const { data } = authClient.useSession();
   return (
     <div className="lowercase">
       protected page
-      {/* {JSON.stringify(data)}
-      {data && <Button onClick={() => authClient.signOut()}>Logout</Button>} */}
+      {JSON.stringify(data)}
+      {data && <Button onClick={() => create.mutate()}>Create</Button>}
     </div>
   );
 };
