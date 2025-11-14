92b3c8d1457ff86b2f0b8c6429d9e7eccc07cb16
B-priyam <priyam3801h@gmail.com>
2025-11-14 16:12:43 +0530

    polar payments integrated

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,9 @@
 import { RegisterForm } from "@/features/auth/components/register-form";
-import { requireAuth } from "@/lib/auth-utils";
+import { requireAuth, requireUnAuth } from "@/lib/auth-utils";
 import React from "react";
 
 const page = async () => {
-  await requireAuth();
+  await requireUnAuth();
   return <RegisterForm />;
 };
 
