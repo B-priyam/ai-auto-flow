81ee57fd7bbbfec3e2527e0dcdc11d824227a8be
B-priyam <priyam3801h@gmail.com>
2025-12-03 15:53:52 +0530

    editor state

--- src/components/base-execution-node.tsx
+++ src/components/base-execution-node.tsx
@@ -1,19 +1,20 @@
 "use client";
 
-import { type NodeProps, Position } from "@xyflow/react";
+import { type NodeProps, Position, useReactFlow } from "@xyflow/react";
 import { type LucideIcon } from "lucide-react";
 import { memo, ReactNode } from "react";
 import { WorkflowNode } from "./workflow-node";
 import { BaseNode, BaseNodeContent } from "./base-node";
 import Image from "next/image";
 import { BaseHandle } from "./base-handle";
+import { NodeStatus, NodeStatusIndicator } from "./node-status-indicator";
 
 interface BaseExecutionNodeProps extends NodeProps {
   icon: LucideIcon | string;
   name: string;
   description?: string;
   children?: ReactNode;
-  //   status?: NodeStatus;
+  status?: NodeStatus;
   onSettings?: () => void;
   onDoubleClick?: () => void;
 }
@@ -27,8 +28,22 @@ export const BaseExecutionNode = memo(
     children,
     onSettings,
     onDoubleClick,
+    status = "initial",
   }: BaseExecutionNodeProps) => {
-    const handleDelete = () => {};
+    const { setNodes, setEdges } = useReactFlow();
+    const handleDelete = () => {
+      setNodes((nodes) => {
+        const updatedNodes = nodes.filter((node) => node.id !== id);
+        return updatedNodes;
+      });
+
+      setEdges((edges) => {
+        const updatedEdges = edges.filter(
+          (edge) => edge.source !== id && edge.target !== id
+        );
+        return updatedEdges;
+      });
+    };
     return (
       <WorkflowNode
         description={description}
@@ -36,26 +51,28 @@ export const BaseExecutionNode = memo(
         name={name}
         onDelete={handleDelete}
       >
-        <BaseNode onDoubleClick={onDoubleClick}>
-          <BaseNodeContent>
-            {typeof Icon === "string" ? (
-              <Image src={Icon} alt={name} width={16} height={16} />
-            ) : (
-              <Icon className="size-4 text-muted-foreground" />
-            )}
-            {children}
-            <BaseHandle
-              id={"target-1"}
-              type="target"
-              position={Position.Left}
-            />
-            <BaseHandle
-              id={"source-1"}
-              type="source"
-              position={Position.Right}
-            />
-          </BaseNodeContent>
-        </BaseNode>
+        <NodeStatusIndicator status={status} variant="border">
+          <BaseNode status={status} onDoubleClick={onDoubleClick}>
+            <BaseNodeContent>
+              {typeof Icon === "string" ? (
+                <Image src={Icon} alt={name} width={16} height={16} />
+              ) : (
+                <Icon className="size-4 text-muted-foreground" />
+              )}
+              {children}
+              <BaseHandle
+                id={"target-1"}
+                type="target"
+                position={Position.Left}
+              />
+              <BaseHandle
+                id={"source-1"}
+                type="source"
+                position={Position.Right}
+              />
+            </BaseNodeContent>
+          </BaseNode>
+        </NodeStatusIndicator>
       </WorkflowNode>
     );
   }
--- src/features/triggers/components/manual-trigger/node.tsx
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -1,18 +1,23 @@
 import { NodeProps } from "@xyflow/react";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { BaseTriggerNode } from "../base-trigger-node";
 import { MousePointerIcon } from "lucide-react";
+import { ManualTriggerDialog } from "./dialog";
 
 export const ManualTriggerNode = memo((props: NodeProps) => {
+  const [open, setOpen] = useState(false);
+  const handleOpenSettings = () => setOpen(true);
+  const nodeStatus = "initial";
   return (
     <>
+      <ManualTriggerDialog open={open} onOpenChange={setOpen} />
       <BaseTriggerNode
         {...props}
         name="When clicking 'Execute workflow'"
         icon={MousePointerIcon}
-        // status={nodeStatus}
-        // onSettings={handleOpenSettings}
-        // onDoubleClick={handleOpenSettings}
+        status={nodeStatus}
+        onSettings={handleOpenSettings}
+        onDoubleClick={handleOpenSettings}
       />
     </>
   );
